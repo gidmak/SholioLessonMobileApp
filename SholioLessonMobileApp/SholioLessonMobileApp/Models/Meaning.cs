15b36b7eef4d4f6a43b3e4af0264322ad023111f
gidmak <gideonmarkus@gmail.com>
2020-06-01 14:44:49 +0100

    Added Meaning Model and deleted MainPage.xaml

---
+++ SholioLessonMobileApp/SholioLessonMobileApp/Models/Meaning.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SholioLessonMobileApp.Models
+{
+    public class Meaning
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string Translation { get; set; }
+        public string RelatedWord { get; set; }
+        public string ImageUrl { get; set; }
+        public string AudioClip { get; set; }
+    }
+}
