2bf5f5ccc752b52de8b71c88f34367e286e913ba
gidmak <gideonmarkus@gmail.com>
2020-06-01 14:59:10 +0100

    Initialize MediaManager and added android project images

--- SholioLessonMobileApp/SholioLessonMobileApp.Android/MainActivity.cs
+++ SholioLessonMobileApp/SholioLessonMobileApp.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using MediaManager;
 
 namespace SholioLessonMobileApp.Droid
 {
@@ -20,6 +21,7 @@ namespace SholioLessonMobileApp.Droid
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            CrossMediaManager.Current.Init(this);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
